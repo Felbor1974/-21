4d85ac716b024133377bd02e6135df53e4f8bf58
felbor <felbor-r@yandex.ru>
2022-03-09 22:37:59 +0300

    second commit

--- src/bookmagazine/Main.java
+++ src/bookmagazine/Main.java
@@ -13,6 +13,7 @@ public class Main {
             System.out.println("j");
             System.out.println("j");
             System.out.println("j");
+            System.out.println("j");
 
 
         }
